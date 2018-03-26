6e0730919a4253266b369f045009e75d8f59dc02
chaofei <chaofeibest@163.com>
2018-03-26 22:36:54 +0800

    上传示例

---
+++ 经典示例/002.运行多个源文件/helloworld.c
@@ -0,0 +1,7 @@
+#include "print.h"
+
+int main(void)
+{
+  printHello();
+  return 0;
+}
---
+++ 经典示例/005.字符的输出/5.c
@@ -0,0 +1,33 @@
+/* */
+#include <stdio.h>
+int main()
+{
+  char ch, nch;  /* */
+  int count;  /* */
+  int k;    /* */
+
+  printf("Please input a string with a # in the end.\n");
+  scanf("%c",&ch);  /* */
+  while(ch != '#')  /* */
+  {
+    if(ch >= '0' && ch <= '9')
+    {
+      /* */
+      count = ch - '0' + 1;  /* */
+      scanf("%c", &nch);  /* */
+      for(k=0; k < count; k++)  /* */
+      {
+        printf("%c", nch);  
+      }
+    }
+    else
+    {
+      printf("%c", ch);  /* */
+    }
+    
+    printf(" ");      /* */
+    scanf("%c", &ch);    /* */
+  }
+  printf("#\n");        /* */
+  return 0;
+}
