2190c2bf8e6068ae706b6c630e557292eed04208
chaofei <chaofeibest@163.com>
2019-04-12 19:41:53 +0800

    修改快速排序的方法,并增加快速排序过程中数组的变化

--- 经典示例/017.对数组元素排序/17.c
+++ 经典示例/017.对数组元素排序/17.c
@@ -109,39 +109,69 @@ void shell_sort(int iarr[], int n) {
   }
 }
 
+/*快速排序介绍
+ * The array before reset is:
+ *                                 8    4    0   -1    6    0   -5    1  -20  200    2   10   53    7    9    8   13   16   28
+ * left: 0 right:18 base: 8 ==>    7    4    0   -1    6    0   -5    1  -20    2    8   10   53  200    9    8   13   16   28 
+ * left: 0 right: 9 base: 7 ==>    2    4    0   -1    6    0   -5    1  -20    7 
+ * left: 0 right: 8 base: 2 ==>  -20    1    0   -1   -5    0    2    6    4 
+ * left: 0 right: 5 base:-20 ==>  -20    1    0   -1   -5    0 
+ * left: 1 right: 5 base: 1 ==>    0    0   -1   -5    1 
+ * left: 1 right: 4 base: 0 ==>   -5    0   -1    0 
+ * left: 1 right: 3 base:-5 ==>   -5    0   -1 
+ * left: 2 right: 3 base: 0 ==>   -1    0 
+ * left: 7 right: 8 base: 6 ==>    4    6 
+ *
+ * 右边的排序
+ * left:11 right:18 base:10 ==>    8    9   10  200   53   13   16   28 
+ * left:11 right:12 base: 8 ==>    8    9 
+ * left:14 right:18 base:200 ==>   28   53   13   16  200 
+ * left:14 right:17 base:28 ==>   16   13   28   53 
+ * left:14 right:15 base:16 ==>   13   16 
+ * Sort Running Time: 19ms
+ * The array after reset is:
+ *                               -20   -5   -1    0    0    1    2    4    6    7    8    8    9   10   13   16   28   53  200
+ *
+ * */
 /*快速排序*/
-void quick_sort(int iarr[], int low, int high) {
-  int start, key; 
-  /*1.排序开始low = 0, high = n - 1*/
-  start = low;
-  key = iarr[low]; /*2.以数组第一个元素为关键数据,赋值给key,即key=iarr[0]*/
-  if (low >= high) { /*如果low >= high说明排序结束了*/
+void quick_sort(int iarr[], int left, int right) {
+  int low = left;
+  int high = right;
+  /*1.排序开始low = 0, high = right -> n - 1*/
+  int key = iarr[left]; /*2.以数组第一个元素为关键数据,赋值给key,即key=iarr[0]*/
+  if (low >= high) { /*递归调用的退出条件*/
     return;
   }
   while (low < high) {
     /*3.从high开始向前搜索,即由后向前搜索(--high),找到第一个小于key的值iarr[high],将iarr[high]和iarr[low]互换*/
-    while (low < high && key <= iarr[high]) {
+    while (low < high && iarr[high] >= key) {
       --high; /*向前寻找*/
     }
-
-    /*3.从low开始向后搜索,即由前向后搜索(++low),找到第一个大于key的值iarr[low],将iarr[low]和iarr[high]互换*/
-    while (low < high && key >= iarr[low]) {
+    /*从右往左寻找到比基线值小的值iarr[high]和基线值所在位置iarr[low]替换,此时iarr[low]既是基线值*/
+    swap(&iarr[low], &iarr[high]);
+    /*3.从左往右开始向后搜索,即由前向后搜索(++low),找到第一个大于key的值iarr[low],将iarr[low]和iarr[high]互换*/
+    while (low < high && iarr[low] <= key) {
       ++low; /*向后寻找*/
     }
-
-    if (key > iarr[high] && key < iarr[low]) {
-      swap(&iarr[low], &iarr[high]);
-      ++low;
-      --high;
-    }
+    /*从左往右寻找到比基线值大的值即iarr[low],此时基线值所在位置下边为high,基线值为iarr[high]*/
+    swap(&iarr[low], &iarr[high]);
   }
-  /*low == high 相遇将基准数和low==high所处的数字交换*/
-  swap(&key, &iarr[low]);
-  /*此时基准左边的都比基准小,基准右边的都比基准大,剩下就是分别左右重复这个过程*/
-  /*用同样的方式对分出来的左边的部分进行同上的做法*/
-  quick_sort(iarr, start, low -1);
-  /*用同样的方式对分出来的右边的部分进行同上的做法*/
-  quick_sort(iarr, low + 1, high);
+  /*此时low==high的值,基准值key为iarr[low],iarr[high],基准值左边所有的值都比基准值小,基准值右边的所有值都比基准值大*/
+  /*基准值左边再重复以上过程*/
+
+  /*
+   * 打印快速排序的过程
+  int i;
+  printf("left:%2d right:%2d base:%2d ==> ", left, right, key);
+  for (i = left; i <= right; ++i) {
+    printf("%4d ", iarr[i]);
+  }
+  printf("\n");
+  */
+
+  quick_sort(iarr, left, low - 1);
+  /*基准值右边再重复以上过程*/
+  quick_sort(iarr, low + 1, right);
 }
 
 int main(void) {
@@ -149,9 +179,9 @@ int main(void) {
   //int s[] = {8, 4, 0, -1, 6, 0, -5, 1, -20, 200, 2, 10, 53, 7, 9};
   int s[] = {8, 4, 0, -1, 6, 0, -5, 1, -20, 200, 2, 10, 53, 7, 9, 8, 13, 16, 28};
   //int s[] = {21, 25, 48, 25, 16, 8};
-  printf("The array before reset is:");
+  printf("The array before reset is:\n%28s", " ");
   for (i = 0; i < sizeof(s)/sizeof(s[0]); ++i) {
-    printf("%4d", s[i]);
+    printf("%5d", s[i]);
   }
   printf("\n");
 
@@ -170,12 +200,13 @@ int main(void) {
   int low = 0;
   int high = n - 1;
   quick_sort(s, low, high);
+
   endtime = clock();
   printf("Sort Running Time: %dms\n", (double)(endtime - begintime)/CLOCKS_PER_SEC);
 
-  printf("The array after reset is:");
+  printf("The array after reset is:\n%28s", " ");
   for (i = 0; i < sizeof(s)/sizeof(s[0]); ++i) {
-    printf("%4d", s[i]);
+    printf("%5d", s[i]);
   }
   printf("\n");
   return 0;
