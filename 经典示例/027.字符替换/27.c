4e5c674606e909e07f5888db3c0e0a14cc449768
chaofei <chaofeibest@163.com>
2019-04-19 10:34:53 +0800

    更新字符替换的函数和注释

--- 经典示例/027.字符替换/27.c
+++ 经典示例/027.字符替换/27.c
@@ -6,21 +6,30 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 #define MAX 50 
 
+/*替换s中的字符,s1中只要在s中出现,都替换成s2对应指针位置的字符,若s2的长度小于s1,赋值的位置的值就可能出现不确定的值*/
 void rep(char *s, char *s1, char *s2) {
   char *p;
   for (; *s; ++s) { /*顺序访问字符串s中的每个字符*/
+    /*获取s1中和s指针位置相同的字符的指针*/
     for (p = s1; *p && *p != *s; ++p); /*检查当前字符串是否在字符串s1中出现*/
     if (*p) {
+      /*将s2指针移动p-s1的位置的值赋值给指针s所在位置*/
       *s = *(p - s1 + s2); /*当前字符串在字符串s1中出现,用字符串s2中的对应字符代替s中的字符*/
     }
   }
 }
 
 int main(void) {
-  char s[MAX]; /*=ABCABC*/
+  /*s="abcabc" s1="b" s2 ="m" result:s=amcamc*/
+  /*s="abcabc" s1="fbac" s2 ="mqr" result:s="rq"*/
+  char s[MAX];
   char s1[MAX], s2[MAX];
+  memset(s, '\0', sizeof(s) / sizeof(s[0]));
+  memset(s1, '\0', sizeof(s1) / sizeof(s1[0]));
+  memset(s2, '\0', sizeof(s2) / sizeof(s2[0]));
   puts("Please input the string for s:");
   scanf("%s", s);
   puts("Please input the string for s1:");
@@ -28,8 +37,10 @@ int main(void) {
   puts("Please input the string for s2:");
   scanf("%s", s2);
 
+  printf("The string before replace:\n");
   printf("s:%s\ts1:%s\ts2:%s\n", s, s1, s2);
   rep(s, s1, s2);
+  printf("The string after replace:\n");
   printf("s:%s\ts1:%s\ts2:%s\n", s, s1, s2);
   return 0;
 }
